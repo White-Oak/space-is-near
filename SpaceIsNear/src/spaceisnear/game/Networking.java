51a1ce4a84cac51e04972d94a58a9214646bbac2
White-Oak <lpzhelud@gmail.com>
2013-07-14 02:33:21 +0000

    Milestone

--- SpaceIsNear/src/spaceisnear/game/Networking.java
+++ SpaceIsNear/src/spaceisnear/game/Networking.java
@@ -1,107 +1,89 @@
 package spaceisnear.game;
 
-import spaceisnear.game.bundles.MessageBundle;
 import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.kryonet.Server;
-import java.io.IOException;
-import lombok.AllArgsConstructor;
+import com.esotericsoftware.kryonet.*;
 import lombok.RequiredArgsConstructor;
-import spaceisnear.game.bundles.Bundle;
+import spaceisnear.game.bundles.*;
 import spaceisnear.game.messages.*;
 
+import java.io.IOException;
+
 /**
- *
  * @author LPzhelud
  */
 @RequiredArgsConstructor public class Networking extends Listener {
 
-    private final GameContext gameContext;
-    private Server server;
-    private Client client;
+	private final GameContext gameContext;
+	private Client client;
 
-    public void host() throws IOException {
-	server = new Server();
-	registerEverything();
-	server.start();
-	server.addListener(this);
-	server.bind(54555);
-    }
-
-    public void connect(String host, int tcpPort) throws IOException {
-	client = new Client();
-	registerEverything();
-	client.start();
-	client.addListener(this);
-	client.connect(5000, host, tcpPort);
-    }
-
-    public void send(NetworkableMessage message) {
-	Bundle bundle = message.getBundle();
-	if (server != null) {
-	    server.sendToAllTCP(bundle);
-	} else if (client != null) {
-	    client.sendTCP(bundle);
+	public void connect(String host, int tcpPort) throws IOException {
+		client = new Client();
+		registerEverything();
+		client.start();
+		client.addListener(this);
+		client.connect(5000, host, tcpPort);
 	}
-	System.out.println("Message sent");
-    }
 
-    @Override
-    public void connected(Connection connection) {
-	connection.sendTCP("Got it");
-	gameContext.sendThemAll(new MessageNetworkState(1));
-    }
-
-    @Override
-    public void disconnected(Connection connection) {
-	gameContext.sendThemAll(new MessageNetworkState(2));
-    }
-
-    @Override
-    public void idle(Connection connection) {
-	gameContext.sendThemAll(new MessageNetworkState(3));
-    }
-
-    @Override
-    public void received(Connection connection, Object object) {
-	if (object instanceof MessageBundle) {
-	    MessageBundle bundle = (MessageBundle) object;
-	    MessageType mt = MessageType.values()[bundle.messageType];
-	    byte[] b = bundle.bytes;
-	    switch (mt) {
-		case MOVED:
-		    MessageMoved mm = MessageMoved.getInstance(b);
-		    gameContext.sendToID(mm, mm.getId());
-		    break;
-	    }
-
-	    System.out.println("Message received");
+	public void send(NetworkableMessage message) {
+		Bundle bundle = message.getBundle();
+		if (client != null) {
+			client.sendTCP(bundle);
+		}
+		System.out.println("Message sent");
 	}
-    }
 
-    public void close() {
-	if (client != null) {
-	    client.close();
-	} else if (server != null) {
-	    server.close();
+	@Override
+	public void connected(Connection connection) {
+		connection.sendTCP("Got it");
+		gameContext.sendThemAll(new MessageNetworkState(1));
 	}
-    }
 
-    public void registerEverything() {
-	register(Bundle.class);
-	register(MessageBundle.class);
-	register(byte[].class);
-    }
-
-    private void register(Class classs) {
-	Kryo kryo = null;
-	if (server != null) {
-	    kryo = server.getKryo();
-	} else if (client != null) {
-	    kryo = client.getKryo();
+	@Override
+	public void disconnected(Connection connection) {
+		gameContext.sendThemAll(new MessageNetworkState(2));
 	}
-	kryo.register(classs);
-    }
+
+	@Override
+	public void idle(Connection connection) {
+		gameContext.sendThemAll(new MessageNetworkState(3));
+	}
+
+	@Override
+	public void received(Connection connection, Object object) {
+		if (object instanceof MessageBundle) {
+			MessageBundle bundle = (MessageBundle) object;
+			MessageType mt = MessageType.values()[bundle.messageType];
+			byte[] b = bundle.bytes;
+			switch (mt) {
+				case MOVED:
+					MessageMoved mm = MessageMoved.getInstance(b);
+					gameContext.sendToID(mm, mm.getId());
+					break;
+			}
+
+			System.out.println("Message received");
+		} else if (object instanceof ObjectBundle) {
+
+		}
+	}
+
+	public void close() {
+		if (client != null) {
+			client.close();
+		}
+	}
+
+	public void registerEverything() {
+		register(Bundle.class);
+		register(MessageBundle.class);
+		register(byte[].class);
+	}
+
+	private void register(Class classs) {
+		Kryo kryo = null;
+		if (client != null) {
+			kryo = client.getKryo();
+		}
+		kryo.register(classs);
+	}
 }
--- SpaceIsNear/src/spaceisnear/server/GameContext.java
+++ SpaceIsNear/src/spaceisnear/server/GameContext.java
@@ -4,46 +4,47 @@
  */
 package spaceisnear.server;
 
-import spaceisnear.server.objects.GameObject;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import lombok.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import spaceisnear.game.messages.Message;
+import spaceisnear.server.objects.GameObject;
 import spaceisnear.server.objects.Player;
 
+import java.util.*;
+
 /**
- *
  * @author LPzhelud
  */
 @RequiredArgsConstructor public class GameContext {
 
-    public static final int TILE_HEIGHT = 16, TILE_WIDTH = 16;
-    @Getter private final Networking networking;
-    @Getter private final List<GameObject> objects;
-    private final List<GameObject> players = new LinkedList<>();
+	public static final int TILE_HEIGHT = 16, TILE_WIDTH = 16;
+	@Getter private final Networking networking;
+	@Getter private final List<GameObject> objects;
+	private final List<GameObject> players = new LinkedList<>();
 
-    public synchronized void sendThemAll(Message m) {
-	for (Iterator<GameObject> it = objects.iterator(); it.hasNext();) {
-	    GameObject gameObject = it.next();
-	    gameObject.message(m);
+	public synchronized void sendThemAll(Message m) {
+		for (Iterator<GameObject> it = objects.iterator(); it.hasNext(); ) {
+			GameObject gameObject = it.next();
+			gameObject.message(m);
+		}
 	}
-    }
 
-    public synchronized void sendToID(Message m, int id) {
-	objects.get(id).message(m);
-    }
+	public synchronized void sendToID(Message m, int id) {
+		objects.get(id).message(m);
+	}
 
-    public synchronized void addObject(GameObject gameObject) {
-	objects.add(gameObject);
-	gameObject.setId(objects.size() - 1);
-    }
+	public synchronized void addObject(GameObject gameObject) {
+		objects.add(gameObject);
+		gameObject.setId(objects.size() - 1);
+	}
 
-    public synchronized void addPlayer() {
-	Player player = new Player(null, this);
-    }
+	public synchronized int addPlayer() {
+		Player player = new Player(null, this);
+		players.add(player);
+		return players.size();
+	}
 
-    public Player getPlayer(int id) {
-	return (Player) players.get(id);
-    }
+	public Player getPlayer(int id) {
+		return (Player) players.get(id);
+	}
 }
