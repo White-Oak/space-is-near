ac4c1377b1b8a7cd795c6be9843287e787772100
White-Oak <lpzhelud@gmail.com>
2014-03-17 22:05:58 +0400

    Animation added. Door item object added. Nothing of these is working though.

--- SpaceIsNear/src/spaceisnear/game/components/client/ItemPaintableComponent.java
+++ SpaceIsNear/src/spaceisnear/game/components/client/ItemPaintableComponent.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import spaceisnear.game.GameContext;
 import spaceisnear.game.components.ComponentType;
 import spaceisnear.game.components.ItemPropertiesComponent;
-import spaceisnear.game.messages.Message;
+import spaceisnear.game.messages.*;
 import spaceisnear.game.messages.properties.MessagePropertySet;
 import spaceisnear.game.objects.items.ItemsArchive;
 import spaceisnear.game.objects.items.StaticItem;
@@ -17,6 +17,10 @@ import spaceisnear.game.objects.items.StaticItem;
 public class ItemPaintableComponent extends PaintableComponent {
 
     private int rotate;
+    private MessageAnimationChanged animation;
+    private int currentStep, currentPosition;
+    private int animationStepsToNextStep;
+    private int currentImageId;
 
     @Override
     public void paintComponent(SpriteBatch batch, int x, int y) {
@@ -24,7 +28,7 @@ public class ItemPaintableComponent extends PaintableComponent {
 	int id = properties.getId();
 	int[] imageIds = ItemsArchive.itemsArchive.getImageIds(id);
 	//curently drawing zero state image
-	TextureRegion image = ItemsArchive.itemsArchive.getTextureRegion(imageIds[0]);
+	TextureRegion image = ItemsArchive.itemsArchive.getTextureRegion(imageIds[currentImageId]);
 	if (rotate != 0) {
 	    batch.draw(image, x, y,
 		    GameContext.TILE_WIDTH >> 1, GameContext.TILE_HEIGHT >> 1,
@@ -43,6 +47,28 @@ public class ItemPaintableComponent extends PaintableComponent {
 		MessagePropertySet mps = (MessagePropertySet) message;
 		if (mps.getName().equals("rotate")) {
 		    rotate = (int) mps.getValue();
+		}
+		break;
+	    case ANIMATION_CHANGED:
+		animation = (MessageAnimationChanged) message;
+		animationStepsToNextStep = animation.getAnimationTime() / MessageAnimationStep.STEP;
+		currentStep = 0;
+		currentPosition = 0;
+		currentImageId = animation.getImageIds()[0];
+		registerForAnimation();
+		break;
+	    case ANIMATION_STEP:
+		if (animation != null) {
+		    currentStep++;
+		    if (currentStep % animationStepsToNextStep == 0) {
+			currentPosition++;
+			if (currentPosition < animation.getImageIds().length) {
+			    currentImageId = animation.getImageIds()[currentPosition];
+			} else {
+			    animation = null;
+			    unregisterForAnimation();
+			}
+		    }
 		}
 		break;
 	}
